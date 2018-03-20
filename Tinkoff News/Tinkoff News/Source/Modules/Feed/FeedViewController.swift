5231499c11798c23bc02893e4a33c8201f0812c5
Olga Shilenko <shilenkooa@gmail.com>
2018-03-20 22:39:10 +0300

    created project in the repository

---
+++ Tinkoff News/Tinkoff News/Source/Modules/Feed/FeedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewController.swift
+//  Tinkoff News
+//
+//  Created by Ольга Шиленко on 20.03.2018.
+//  Copyright © 2018 Ольга Шиленко. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Tinkoff News/Tinkoff News/Source/Modules/News/NewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsViewController.swift
+//  Tinkoff News
+//
+//  Created by Ольга Шиленко on 20.03.2018.
+//  Copyright © 2018 Ольга Шиленко. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

6965be5a194577714693d6ddc8217144bb521033
Olga Shilenko <shilenkooa@gmail.com>
2018-03-25 23:53:11 +0300

    added RequestManager to the project

--- Tinkoff News/Tinkoff News/Source/AppDelegate.swift
+++ Tinkoff News/Tinkoff News/Source/AppDelegate.swift
@@ -14,9 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        configureRequestManager()
+        
         return true
     }
 
@@ -89,5 +90,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+}
+
+// MARK: - Private methods
+private extension AppDelegate {
+    func configureRequestManager() {
+        RequestManager.shared.isPrintedLogs = true
+    }
 }
 
---
+++ Tinkoff News/Tinkoff News/Source/Common/Helpers/RequestManager.swift
@@ -0,0 +1,124 @@
+//
+//  RequestManager.swift
+//  Tinkoff News
+//
+//  Created by Ольга Шиленко on 24.03.2018.
+//  Copyright © 2018 Ольга Шиленко. All rights reserved.
+//
+
+import Foundation
+
+typealias Headers = [String: String]
+typealias Parameters = [String: Any]
+typealias Success<T> = (T) -> Void
+typealias Failure = (Error?) -> Void
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+final class RequestManager: NSObject {
+    static var shared: RequestManager = RequestManager()
+    
+    // MARK: - Public variables
+    var isPrintedLogs: Bool = false
+    
+    // MARK: - Pivate variables
+    private var session: URLSession!
+    
+    private override init() {
+        super.init()
+        
+        let configuration = URLSessionConfiguration.default
+        session = URLSession(configuration: configuration)
+    }
+}
+
+extension RequestManager {
+    func requestObject<T>(method: HTTPMethod = .get,
+                          baseURL: String,
+                          path: String,
+                          headers: Headers? = nil,
+                          parameters: Parameters? = nil,
+                          success: @escaping Success<T>,
+                          failure: @escaping Failure) -> URLSessionDataTask? {
+        guard let url = URL(string: "\(baseURL)\(path)") else {
+            print("Can't create URL for baseURL: \(baseURL) and path: \(path)")
+            return nil
+        }
+        
+        let urlRequest = configureUrlRequest(url: url, parameters: parameters, method: method)
+        printLogs(urlRequest.description)
+        let task = session.dataTask(with: urlRequest, completionHandler: { [weak self] (data, response, error) in
+            guard let strongSelf = self else { return }
+            
+            let decoder = JSONDecoder()
+            
+            if let error = error {
+                failure(error)
+            } else if let response = response, let httpResponse = response as? HTTPURLResponse,
+                ([Int](200..<300)).contains(where: { $0 == httpResponse.statusCode }) {
+                strongSelf.printLogs(response.description)
+                
+                if let data = data, let jsonResponse = try? decoder.decode(ResponseObject.self, from: data) {
+                    if let object = jsonResponse.payload as? T {
+                        success(object)
+                    }
+                } else {
+                    failure(Constants.ServerError.JSONDecoding)
+                }
+            } else {
+                failure(Constants.ServerError.nilResponse)
+            }
+        })
+        task.resume()
+        
+        return task
+    }
+}
+
+private extension RequestManager {
+    func configure(parameters: Parameters) -> String {
+        var components: [(String, String)] = []
+        
+        parameters.forEach { (key, value) in
+            if let value = value as? Int {
+                components.append((key, "\(value)"))
+            }
+        }
+        
+        return components.map { "\($0)=\($1)" }.joined(separator: "&")
+    }
+    
+    func configureUrlRequest(url: URL, parameters: Parameters?, method: HTTPMethod) -> URLRequest {
+        var urlRequest = URLRequest(url: url,
+                                    cachePolicy: session.configuration.requestCachePolicy,
+                                    timeoutInterval: session.configuration.timeoutIntervalForRequest)
+        
+        if let parameters = parameters {
+            switch method {
+            case .get:
+                if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
+                    !parameters.isEmpty {
+                    urlComponents.percentEncodedQuery = configure(parameters: parameters)
+                    urlRequest.url = urlComponents.url
+                }
+            case .post:
+                if urlRequest.value(forHTTPHeaderField: "Content-Type") == nil {
+                    urlRequest.setValue("application/x-www-form-urlencoded; charset=utf-8",
+                                        forHTTPHeaderField: "Content-Type")
+                }
+                
+                let parametersString = configure(parameters: parameters)
+                urlRequest.httpBody = parametersString.data(using: .utf8, allowLossyConversion: false)
+            }
+        }
+        
+        return urlRequest
+    }
+    
+    func printLogs(_ string: String) {
+        print("[RequestManager] \(Date()): \(string)")
+    }
+}

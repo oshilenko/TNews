98a2d53847032a1b1e67bb9ea78ea95bdc00b370
Olga Shilenko <shilenkooa@gmail.com>
2018-03-26 23:32:54 +0300

    added StringCalculateHeightProtocol to the project for calculating string height

---
+++ Tinkoff News/Tinkoff News/Source/Common/Protocols/StringCalculateHeightProtocol.swift
@@ -0,0 +1,53 @@
+//
+//  StringCalculateHeightProtocol.swift
+//  Tinkoff News
+//
+//  Created by Ольга Шиленко on 26.03.2018.
+//  Copyright © 2018 Ольга Шиленко. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol StringCalculateHeightProtocol {
+    var font: UIFont { get }
+    var width: CGFloat { get }
+    var size: CGSize { get }
+}
+
+public enum StringItemType {
+    case feedItemTitle(text: String)
+}
+
+// MARK: - StringCalculateHeightProtocol methods
+extension StringItemType: StringCalculateHeightProtocol {
+    var font: UIFont {
+        return UIFont(name: "Helvetica Neue medium", size: 17)!
+    }
+    
+    var width: CGFloat {
+        return UIScreen.main.bounds.size.width - 32
+    }
+    
+    var size: CGSize {
+        switch self {
+        case .feedItemTitle(let text):
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineBreakMode = .byWordWrapping
+            
+            let attrStr = NSAttributedString(string: text,
+                                             attributes: [NSAttributedStringKey.font: font,
+                                                          NSAttributedStringKey.paragraphStyle: paragraphStyle])
+            
+            let textSize = attrStr
+                .boundingRect(with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
+                              options: [.usesLineFragmentOrigin, .usesFontLeading],
+                              context: nil).size
+            
+            return textSize
+        }
+    }
+    
+    
+}
+
